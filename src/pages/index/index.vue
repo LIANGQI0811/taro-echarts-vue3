4938ffd115085f15961be7b3fd73225c1ad11012
liangqi <watsonl@qq.com>
2023-07-26 21:10:05 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <view class="index">
+    <text>{{msg}}</text>
+    <avatar />
+    <navigator url='plugin://myPlugin/list'>
+      Go to pages/list!
+    </navigator>
+  </view>
+</template>
+
+<script>
+import { ref } from 'vue'
+import './index.scss'
+
+export default {
+  setup () {
+    const msg = ref('Hello world')
+    return {
+      msg
+    }
+  }
+}
+</script>
---
+++ src/plugin/components/listItem/listItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <view>
+    <view>name: {{name}}</view>
+    <view>value: {{value}}</view>
+  </view>
+</template>
+
+<script>
+import { ref } from 'vue'
+import './listItem.scss'
+export default {
+  props: ['name', 'value']
+}
+</script>
